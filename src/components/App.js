4290963aa898d968f2e62dc426ce41675d7acd5e
PironSteve <steve.piron@gmail.com>
2019-03-13 11:58:13 +0100

    img fix

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     return (
       <Router>
         <div>
-          <Switch>
+          <Switch basename="/resume">
             <Route
               exact
               path={`${process.env.BASE_PATH || ""}/`}
