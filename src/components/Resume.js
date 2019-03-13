467555b4b0b1e2bfc03db4a3b44b4218e5f0be98
PironSteve <steve.piron@gmail.com>
2019-03-13 12:20:45 +0100

    img fix

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -146,8 +146,11 @@ export default class Resume extends React.Component {
           </div>
           <section id="about" className="container-fluid">
             <div className="col-xs-8 col-md-4 profile-picture">
-              path={`${process.env.BASE_PATH || ""}/${me}`}
-              <img src={} alt="Piron Steve" className="img-circle" />
+              <img
+                src={`${process.env.BASE_PATH || ""}/${me}`}
+                alt="Piron Steve"
+                className="img-circle"
+              />
             </div>
             <div className="heading">
               <h1>Hello, moi c'est Steve</h1>
