47e27f9013901ed1485fc890f4196622c916acd7
PironSteve <steve.piron@gmail.com>
2019-03-15 09:14:10 +0100

    Site V1.0

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -147,11 +147,7 @@ export default class Resume extends React.Component {
           </div>
           <section id="about" className="container-fluid">
             <div className="col-xs-8 col-md-4 profile-picture">
-              <img
-                src={`${process.env.BASE_PATH || ""}/${me}`}
-                alt="Piron Steve"
-                className="img-circle"
-              />
+              <img src={me} alt="Piron Steve" className="img-circle" />
             </div>
             <div className="heading">
               <h1>Hello, moi c'est Steve</h1>
