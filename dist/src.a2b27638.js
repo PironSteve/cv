990cf2ea78d8dc30a43bc07abd1520b37a80f03d
PironSteve <steve.piron@gmail.com>
2019-03-29 12:28:55 +0100

    responsive fix

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -33496,7 +33496,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40397" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39995" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
